a716c22deba3bbd41669874edd44f7b8f9f858c5
Stanford <spen0022@student.monash.edu>
2020-10-23 22:15:10 +1100

    Initial Commit

---
+++ TreasureHunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TreasureHunter
+//
+//  Created by Stanford on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
